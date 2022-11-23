30d17e15d66c5a307d54f437d1040ccfae5a179e
Ibrahim Enes Hayber <100522586+whis-uas@users.noreply.github.com>
2022-11-23 16:52:44 +0100

    first commit

---
+++ whichisgreater.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    cin >> a;
+    cin >> b;
+    if(a > b)
+        cout << 1;
+    else
+        cout << 0;
+    return 0;
+}

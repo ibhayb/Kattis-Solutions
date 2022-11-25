7bc6fbbf1f926d5588452c60f0de8f8e7d8e5bcf
Ibrahim Enes Hayber <100522586+whis-uas@users.noreply.github.com>
2022-11-25 18:43:14 +0100

    added solution

---
+++ fyi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    cin >> a;
+    if( a/10000 == 555)
+        cout << "1";
+    else
+        cout << "0";
+    return 0;
+}
